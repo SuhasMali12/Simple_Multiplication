d08c2d3de710b6f5aa1a052b47ba606d1e5bd1c6
Suhas Mali <suhasmali456@gmail.com>
2024-05-16 20:45:26 +0530

    Add files via upload

---
+++ SimpleMutliplicationProject/src/controller/Controller.java
@@ -0,0 +1,16 @@
+package controller;
+
+import service.Service;
+public class Controller {
+	
+void m1() {
+    	Service ss =new Service();
+    	int c=ss.m1();
+         System.out.println(c);
+    }
+public static void main(String[] args) {
+	Controller cc=new Controller();
+	cc.m1();
+}
+
+}
---
+++ SimpleMutliplicationProject/src/dao/Dao.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class Dao {
+	
+	int a=10;
+	int b=5;
+	
+public int m1() {
+    int c=a*b;
+    return c;
+}
+
+}
